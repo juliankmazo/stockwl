ee00903192776dbe8fb5c7be1bf6c1a836888994
juanesarango <juanes.ao@gmail.com>
2014-11-26 13:11:08 -0500

    mejoras en el google query

--- backend/core/controllers/google_finance.py
+++ backend/core/controllers/google_finance.py
@@ -1,17 +1,27 @@
 from lib import requests
 from bs4 import BeautifulSoup
 
-# from models.stock import Stock
-query_stock = 'gmx.ax'
-query_keys = ['eps', 'shares', 'pe_ratio']
+stock = 'gmx'
+keys = ['eps','pe_ratio','latest_dividend-dividend_yield', 'shares']
 
-def gf_query(query_stock, query_keys=[]):
-
-	r = requests.get('https://www.google.com/finance?q=' + query_stock)
+def gf_query(query_stock, query_keys=keys):
+	r = requests.get('https://www.google.com/finance?q=ASX:'+query_stock)
+	stocks = {}
 	if r.status_code==200:
 		body = BeautifulSoup(r.text)
-		for key in query_keys:
-			element = body.find(attrs={"data-snapfield": key})
-			print element
-			value = element.parent.find(class_="val").string if element else ""
-			print  u'{key}: {value}'.format(key=key, value=value)
+		if body.find(id='companyheader'):
+			stocks['name'] = body.find(id='companyheader').h3.string
+			stocks['code'] = body.find(id='companyheader').div.next_sibling.a.string
+			stocks['price'] = body.find(id="price-panel").div.span.span.string
+			for key in query_keys:
+				element = body.find(attrs={"data-snapfield": key})
+				value = element.parent.find(class_="val").string if element else ""
+				stocks[key] = value
+				# try:
+				# 	stocks[key] = float(value)
+				# except:
+				# 	if key == 'shares':
+				# 		stocks[key] = float(value[:-3])*1e6
+				# 	else:
+				# 		stocks[key] = 'N/A'
+	return stocks
---
+++ backend/tools/upload_production.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# # declare STRING variable
+# STRING="Hello Diable :*"
+# #print variable on screen
+# echo $STRING
+
+ appcfg.py --email=juanes.ao@gmail.com update stockwl/backend
+
+ dev_appserver.py .

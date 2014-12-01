cdb244e0e2e1c4359a0df943bd2c80873d22907a
Julian <julian.kmazo@gmail.com>
2014-12-01 17:55:54 -0500

    Sorting

--- frontend/app/controllers/stocks.js
+++ frontend/app/controllers/stocks.js
@@ -7,6 +7,28 @@ export default Ember.ArrayController.extend({
 		sortBy: function(property) {
 			this.set('sortProperties', [property]);
 			this.set('sortAscending', !this.get('sortAscending'));
+			var stocks = ['code',
+						  'companyName',
+						  'currentPrice',
+						  'pe',
+						  'priceSales',
+						  'eps',
+						  'totalCashPerShare',
+						  'bookValuePerShare',
+						  'dividendYield',
+						  'profitMargin',
+						  'totalDebt',
+						  'notes',
+						  'yearsDebt',
+						  'shares'];
+			for (var i = stocks.length - 1; i >= 0; i--) {
+				this.set(stocks[i]+'UpDown', '');
+			};
+			if (this.get('sortAscending')) {
+				this.set(property+'UpDown', 'glyphicon-chevron-up');
+			}else {
+				this.set(property+'UpDown', 'glyphicon-chevron-down');
+			}
 		}
 	}
 });
--- frontend/app/serializers/application.js
+++ frontend/app/serializers/application.js
@@ -16,5 +16,9 @@ export default DS.RESTSerializer.extend({
       // store.metaForType(type, { total: payload.etag });  // sets the metadata for "post"
       delete payload.etag;  // keeps ember data from trying to parse "total" as a record
     }
+
+    if (!payload.stocks) {
+      payload.stocks=[];
+    }
   }
 });
